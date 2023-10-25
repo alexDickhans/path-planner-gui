25d714835e46e1f7ba3957b6fcdf40a69aa6a98d
alexDickhans <55064247+alexDickhans@users.noreply.github.com>
2023-10-25 14:04:37 -0600

    All working

---
+++ test2.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+std::vector<std::pair<PathPlanner::BezierSegment, Pronounce::SinusoidalVelocityProfile*>> Path2 = {{PathPlanner::BezierSegment(
+PathPlanner::Point(9.88701_in, 79.096_in),
+PathPlanner::Point(9.88701_in, 19.774_in),
+PathPlanner::Point(24.7175_in, 14.8305_in),
+PathPlanner::Point(39.548_in, 9.88701_in)
+),
+nullptr},
+{PathPlanner::BezierSegment(
+PathPlanner::Point(39.548_in, 9.88701_in),
+PathPlanner::Point(54.3785_in, 4.9435_in),
+PathPlanner::Point(81.1723_in, 51.8079_in),
+PathPlanner::Point(107.966_in, 98.6723_in)
+),
+nullptr},
+};
+// PathPlanner made path
