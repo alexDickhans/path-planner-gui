aabdff8904c8037c3034d36d02977d3ce2ea903f
Alex Dickhans <adickhans@gmail.com>
2024-02-02 20:36:02 -0700

    Testing on mac

---
+++ build/CMakeFiles/path_planner_gui.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/alex/CLionProjects/path-planner-gui/ImGuiFileDialog/ImGuiFileDialog.cpp" "CMakeFiles/path_planner_gui.dir/ImGuiFileDialog/ImGuiFileDialog.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/ImGuiFileDialog/ImGuiFileDialog.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/imgui.cpp" "CMakeFiles/path_planner_gui.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/imgui.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/imgui_demo.cpp" "CMakeFiles/path_planner_gui.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/imgui_demo.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/imgui_draw.cpp" "CMakeFiles/path_planner_gui.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/imgui_draw.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/imgui_tables.cpp" "CMakeFiles/path_planner_gui.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/imgui_tables.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/imgui_widgets.cpp" "CMakeFiles/path_planner_gui.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/imgui_widgets.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/imgui/misc/cpp/imgui_stdlib.cpp" "CMakeFiles/path_planner_gui.dir/imgui/misc/cpp/imgui_stdlib.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/imgui/misc/cpp/imgui_stdlib.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/implot/implot.cpp" "CMakeFiles/path_planner_gui.dir/implot/implot.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/implot/implot.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/implot/implot_items.cpp" "CMakeFiles/path_planner_gui.dir/implot/implot_items.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/implot/implot_items.cpp.o.d"
+  "/Users/alex/CLionProjects/path-planner-gui/main.cpp" "CMakeFiles/path_planner_gui.dir/main.cpp.o" "gcc" "CMakeFiles/path_planner_gui.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/path_planner_gui.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path_planner_gui.dir/ImGuiFileDialog/ImGuiFileDialog.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/ImGuiFileDialog/ImGuiFileDialog.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/imgui/misc/cpp/imgui_stdlib.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/imgui/misc/cpp/imgui_stdlib.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/implot/implot.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/implot/implot.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/implot/implot_items.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/implot/implot_items.cpp.o.d"
+  "CMakeFiles/path_planner_gui.dir/main.cpp.o"
+  "CMakeFiles/path_planner_gui.dir/main.cpp.o.d"
+  "path_planner_gui"
+  "path_planner_gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path_planner_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
